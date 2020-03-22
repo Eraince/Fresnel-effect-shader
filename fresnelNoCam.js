c3c7db46b26da3b3c61625696868c678937bf92a
Eraince <eraince@hotmail.com>
2020-03-22 18:28:45 -0400

    first update.

---
+++ fresnelNoCam.js
@@ -0,0 +1,150 @@
+import * as THREE from "../resources/three.module.js";
+import { OrbitControls } from "../resources/examples/jsm/controls/OrbitControls.js";
+
+const vshader = `
+
+
+    uniform float mRefractionRatio;
+		uniform float mFresnelBias;
+		uniform float mFresnelScale;
+		uniform float mFresnelPower;
+
+    varying vec3 vReflect;
+		varying vec3 vRefract;
+		varying float vReflectionFactor;
+
+
+    void main() {
+
+			vec4 mvPosition = modelViewMatrix * vec4( position, 1.0 );
+			vec4 worldPosition = modelMatrix * vec4( position, 1.0 );
+
+      vec3 worldNormal = normalize( mat3( modelMatrix[0].xyz, modelMatrix[1].xyz, modelMatrix[2].xyz ) * normal );
+      
+      //calculate incidence director
+			vec3 I = worldPosition.xyz - cameraPosition;
+
+      //calculate reflected light
+      vReflect = reflect( I, worldNormal );
+      
+      //calculate refracted light
+      vRefract = refract( normalize( I ), worldNormal, mRefractionRatio );
+      
+      //calculate fresnel effect
+			vReflectionFactor = mFresnelBias + mFresnelScale * pow( 1.0 + dot( normalize( I ), worldNormal ), mFresnelPower );
+
+			gl_Position = projectionMatrix * mvPosition;
+ 
+		}
+
+
+`;
+const fshader = `
+    uniform samplerCube tCube;
+
+		varying vec3 vReflect;
+		varying vec3 vRefract;
+		varying float vReflectionFactor;
+
+    void main() {
+
+      //sample from skybox texture
+      vec4 reflectedColor = textureCube( tCube, vec3( vReflect.x, vReflect.yz ) );
+      
+			vec4 refractedColor = vec4( 1.0 );
+			refractedColor.rgb = textureCube( tCube, vec3( vRefract.x, vRefract.yz ) ).rgb;
+			
+      //gl_FragColor = reflectedColor;
+      //gl_FragColor = refractedColor;
+      gl_FragColor = mix( refractedColor, reflectedColor, clamp( vReflectionFactor, 0.0, 1.0 ) );
+
+		}
+`;
+
+const main = () => {
+  const canvas = document.querySelector("#c");
+  const renderer = new THREE.WebGLRenderer({ canvas });
+  const scene = new THREE.Scene();
+
+  //set up camera
+  const camera = new THREE.PerspectiveCamera(
+    60,
+    window.innerWidth / window.innerHeight,
+    1,
+    100000
+  );
+  camera.position.z = 3200;
+
+  //set up orbitcontrol
+
+  const controls = new OrbitControls(camera, canvas);
+  controls.target.set(0, 5, 0);
+  controls.update();
+
+  {
+    //load and create skybox
+    const urls = [
+      "../skybox/right.png",
+      "../skybox/left.png",
+      "../skybox/top.png",
+      "../skybox/bottom.png",
+      "../skybox/front.png",
+      "../skybox/back.png"
+    ];
+
+    const textureCube = new THREE.CubeTextureLoader().load(urls);
+    scene.background = textureCube;
+
+    //create shader material and apply to geometry
+
+    const uniforms = {
+      mRefractionRatio: { value: 1.2 },
+      mFresnelBias: { value: 0.1 },
+      mFresnelPower: { value: 1.0 },
+      mFresnelScale: { value: 1.0 },
+      tCube: { value: textureCube }
+    };
+
+    const geometry = new THREE.SphereBufferGeometry(100, 32, 16);
+    const material = new THREE.ShaderMaterial({
+      uniforms: uniforms,
+      vertexShader: vshader,
+      fragmentShader: fshader
+    });
+
+    for (var i = 0; i < 50; i++) {
+      var mesh = new THREE.Mesh(geometry, material);
+      mesh.position.x = Math.random() * 10000 - 5000;
+      mesh.position.y = Math.random() * 10000 - 5000;
+      mesh.position.z = Math.random() * 10000 - 5000;
+
+      mesh.scale.x = mesh.scale.y = mesh.scale.z = Math.random() * 3 + 1;
+      scene.add(mesh);
+    }
+  }
+
+  const render = () => {
+    if (resizeRendererToDisplaySize(renderer)) {
+      const canvas = renderer.domElement;
+      camera.aspect = canvas.clientWidth / canvas.clientHeight;
+      camera.updateProjectionMatrix();
+    }
+    renderer.render(scene, camera);
+    requestAnimationFrame(render);
+  };
+
+  requestAnimationFrame(render);
+
+  const resizeRendererToDisplaySize = renderer => {
+    const canvas = renderer.domElement;
+    const width = canvas.clientWidth;
+    const height = canvas.clientHeight;
+    const needResize = canvas.width != width || canvas.height != height;
+    if (needResize) {
+      renderer.setSize(width, height, false);
+    }
+    return needResize;
+  };
+};
+
+main();
---
+++ script.js
@@ -0,0 +1,174 @@
+import * as THREE from "../resources/three.module.js";
+import { OrbitControls } from "../resources/examples/jsm/controls/OrbitControls.js";
+
+import { DragControls } from "../resources/examples/jsm/controls/DragControls.js";
+const vshader = `
+
+
+uniform float mRefractionRatio;
+uniform float mFresnelBias;
+uniform float mFresnelScale;
+uniform float mFresnelPower;
+
+varying vec3 vReflect;
+varying vec3 vRefract;
+varying float vReflectionFactor;
+
+
+void main() {
+
+  vec4 mvPosition = modelViewMatrix * vec4( position, 1.0 );
+  vec4 worldPosition = modelMatrix * vec4( position, 1.0 );
+
+  vec3 worldNormal = normalize( mat3( modelMatrix[0].xyz, modelMatrix[1].xyz, modelMatrix[2].xyz ) * normal );
+
+  vec3 I = worldPosition.xyz - cameraPosition;
+
+  vReflect = reflect( I, worldNormal );
+  vRefract = refract( normalize( I ), worldNormal, mRefractionRatio );
+  
+  vReflectionFactor = mFresnelBias + mFresnelScale * pow( 1.0 + dot( normalize( I ), worldNormal ), mFresnelPower );
+
+  gl_Position = projectionMatrix * mvPosition;
+
+}`;
+const fshader = `
+uniform samplerCube tCube;
+
+varying vec3 vReflect;
+varying vec3 vRefract;
+varying float vReflectionFactor;
+
+void main() {
+
+  vec4 reflectedColor = textureCube( tCube, vec3( vReflect.x, vReflect.yz ) );
+
+  vec4 refractedColor = vec4( 1.0 );
+
+  refractedColor.rgb = textureCube( tCube, vec3( vRefract.x, vRefract.yz ) ).rgb;
+  
+
+  gl_FragColor = mix( refractedColor, reflectedColor, clamp( vReflectionFactor, 0.0, 1.0 ) );
+  
+ 
+}`;
+
+const main = () => {
+  const canvas = document.querySelector("#c");
+
+  const renderer = new THREE.WebGLRenderer({ canvas });
+
+  // set up camera
+  const fov = 45;
+  const aspect = 2; // the canvas default
+  const near = 0.1;
+  const far = 10000;
+  const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+  camera.position.set(0, 0, -200);
+  const controls = new OrbitControls(camera, canvas);
+  controls.target.set(0, 5, 0);
+  controls.update();
+
+  //set up scene
+
+  const scene = new THREE.Scene();
+  let bSphereCamera = new THREE.CubeCamera(0.1, 500, 1000);
+  scene.add(bSphereCamera);
+
+  //add a ambient light
+
+  {
+    const color = 0xffffff;
+    const intensity = 1;
+    const light = new THREE.DirectionalLight(color, intensity);
+    light.position.set(-1, 2, 4);
+
+    scene.add(light);
+  }
+
+  //load and creat skybox
+
+  {
+    const skyLoader = new THREE.CubeTextureLoader();
+    const texture = skyLoader.load([
+      "../skybox-2/BrightMorning01_LF.png",
+      "../skybox-2/BrightMorning01_RT.png",
+      "../skybox-2/BrightMorning01_UP.png",
+      "../skybox-2/BrightMorning01_DN.png",
+      "../skybox-2/BrightMorning01_FR.png",
+      "../skybox-2/BrightMorning01_BK.png"
+    ]);
+    scene.background = texture;
+  }
+
+  // -----------------------------------------
+
+  {
+    const cubeSize = 25;
+    const cubeGeo = new THREE.BoxBufferGeometry(cubeSize, cubeSize, cubeSize);
+    const cubeMat = new THREE.MeshPhongMaterial({ color: "#8AC" });
+    const mesh = new THREE.Mesh(cubeGeo, cubeMat);
+    mesh.position.set(75, 0, 0);
+    scene.add(mesh);
+  }
+
+  // ----------------------------------------------------------------------
+  {
+    let uniforms = {
+      mRefractionRatio: { value: 1.02 },
+
+      mFresnelBias: { value: 0.1 },
+      mFresnelPower: { value: 3.5 },
+      mFresnelScale: { value: 1.0 },
+
+      tCube: { type: "t", value: bSphereCamera.renderTarget }
+    };
+
+    // create custom material for the shader
+
+    let customMaterial = new THREE.ShaderMaterial({
+      uniforms: uniforms,
+
+      vertexShader: vshader,
+
+      fragmentShader: fshader
+    });
+    let bSphere = new THREE.Mesh(
+      new THREE.SphereGeometry(50, 32, 32),
+      customMaterial
+    );
+
+    bSphere.position.set(0, 0, 0);
+    scene.add(bSphere);
+    bSphereCamera.position.set(0, 0, 0);
+  }
+
+  //add animation loop
+
+  function render() {
+    if (resizeRendererToDisplaySize(renderer)) {
+      const canvas = renderer.domElement;
+      camera.aspect = canvas.clientWidth / canvas.clientHeight;
+      camera.updateProjectionMatrix();
+    }
+    bSphereCamera.update(renderer, scene);
+    renderer.render(scene, camera);
+    requestAnimationFrame(render);
+  }
+  requestAnimationFrame(render);
+
+  //responsive resize of canvas
+
+  function resizeRendererToDisplaySize(renderer) {
+    const canvas = renderer.domElement;
+    const width = canvas.clientWidth;
+    const height = canvas.clientHeight;
+    const needResize = canvas.width != width || canvas.height != height;
+    if (needResize) {
+      renderer.setSize(width, height, false);
+    }
+    return needResize;
+  }
+};
+
+main();
